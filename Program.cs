6312fba68c47e18d29361d043c2982ad56381959
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-07-25 07:59:07 +0300

    dz_32
    dz_32

--- Program.cs
+++ Program.cs
@@ -7,23 +7,24 @@ namespace vn_mode_csharp_dz32
         static void Main(string[] args)
         {
             int[] userArray = new int[]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
             Shuffle(userArray);
+
             foreach (int number in userArray)
             {
                 Console.Write(number + " ");
             }
+        }
 
-            static void Shuffle(int[] array)
+        static void Shuffle(int[] array)
+        {
+            Random random = new Random();
+
+            for (int i = array.Length - 1; i >= 1; i--)
             {
-                Random random = new Random();
-                for (int i = array.Length -1 ; i >= 1; i--)
-                {
-                    int j = random.Next(i + 1);
-                    int temp = array[j];
-                    array[j] = array[i];
-                    array[i] = temp;
-                }
+                int randomNumber = random.Next(i + 1);
+                int tempNumber = array[randomNumber];
+                array[randomNumber] = array[i];
+                array[i] = tempNumber;
             }
         }
     }
